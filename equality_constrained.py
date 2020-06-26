10fc80f68e3a8eca6839d75f43c9bd8f38ef2c84
Imam Mashur <58927519+imammashur@users.noreply.github.com>
2020-06-26 23:00:07 +0800

    Create equality_constrained.py

---
+++ equality_constrained.py
@@ -0,0 +1,13 @@
+from cvxopt import solvers, matrix, spdiag, log
+
+def acent(A, b):
+    m, n = A.size
+    def F(x=None, z=None):
+        if x is None: return 0, matrix(1.0, (n,1))
+        if min(x) <= 0.0: return None
+        f = -sum(log(x))
+        Df = -(x**-1).T
+        if z is None: return f, Df
+        H = spdiag(z[0] * x**-2)
+        return f, Df, H
+    return solvers.cp(F, A=A, b=b)['x']
